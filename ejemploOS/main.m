99ecabedf16634b0e59a232afa572ffdd0e122f3
user138066 <user138066@eu249.local>
2018-04-12 16:35:34 +0100

    Initial Commit

---
+++ ejemploOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ejemploOS
+//
+//  Created by user138066 on 4/12/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
